b45b7c0917bf2563d03c3e782ca885abe1441b65
huyida <huyidada@163.com>
2018-11-16 16:28:51 +0900

    Add Descripation

--- USERPRO/charset.py
+++ USERPRO/charset.py
@@ -5,19 +5,20 @@
 # @Site    :
 # @File    : charset.py
 # @Software: PyCharm
+# @Description: 文件由JIS编码转UTF-8格式
 import codecs
 
-
+# 读取文件
 def ReadFile(filePath, encoding="shift-jis"):
     with codecs.open(filePath, "r", encoding) as f:
         return f.read()
 
-
+# 写文件
 def WriteFile(filePath, u, encoding="utf-8"):
     with codecs.open(filePath, "w", encoding) as f:
         f.write(u)
 
-
+# JIS转UTF-8
 def JIS_2_UTF8(src, dst):
     content = ReadFile(src, encoding="shift-jis")
     WriteFile(dst, content, encoding="utf-8")
